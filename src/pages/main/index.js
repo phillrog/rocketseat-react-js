0f89e2a6e1a8c26a6632189cf261e48f9a5e76c5
fh.proger <phillipe.souza@rte.com.br>
2020-04-09 18:10:12 -0300

    Armazenando estado

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,18 +2,29 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
-
+    state = {
+        products: []
+    }
+    
     componentDidMount(){
         this.loadProducts();
     }
 
     render() {
-        return <h1>Hello World</h1>;
+        return (
+            <div className="product-list">
+                { 
+                    this.state.products.map(product => (
+                    <h2 key={product._id}>{product.title}</h2>
+                    ))
+                }
+            </div>
+        )
     }
 
     loadProducts = async () => {
         const response = await api.get(`/products`);
 
-        console.log(response.data.docs);
+        this.setState( { products: response.data.docs});
     }
 }
